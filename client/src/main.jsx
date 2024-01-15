ea3bba1a37e325fe37d08afcef812800b1b3ab1f
Melody Ho <hyho.melody@gmail.com>
2024-01-15 14:26:22 -0800

    Implement basic CSS reset and define CSS variables for color palette

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,11 @@
+/// Imports ///
+// external
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+// components
 import Router from "./Router";
+// assets
+import "./global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
