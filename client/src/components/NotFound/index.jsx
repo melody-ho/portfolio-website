98ed490e3160b844987814d0874591a358e1f1e2
Melody Ho <hyho.melody@gmail.com>
2024-01-29 12:16:40 -0500

    Add not-found page

--- client/src/components/NotFound/index.jsx
+++ client/src/components/NotFound/index.jsx
@@ -1,5 +1,32 @@
+/// Imports ///
+// external
+import { useContext, useEffect, useState } from "react";
+// contexts
+import ThemeContext from "../../ThemeContext";
+// assets
+import darkTheme from "./dark.module.css";
+import lightTheme from "./light.module.css";
+
+/// Component ///
 function NotFound() {
-  return <>Not Found</>;
+  // initialize theme //
+  const theme = useContext(ThemeContext);
+
+  // initialize CSS module //
+  const [s, setS] = useState(theme === "light" ? lightTheme : darkTheme);
+
+  // change CSS module when theme is changed //
+  useEffect(() => {
+    setS(theme === "light" ? lightTheme : darkTheme);
+  }, [theme]);
+
+  // render //
+  return (
+    <p className={s.message}>
+      <span className={s.messageLarge}>Oops!</span>
+      <span className={s.messageSmall}>This page does not exist.</span>
+    </p>
+  );
 }
 
 export default NotFound;
--- client/src/components/TabletOrMobile/index.jsx
+++ client/src/components/TabletOrMobile/index.jsx
@@ -1,11 +1,13 @@
 /// Imports ///
 // external
+import { useEffect, useState } from "react";
 import { useMediaQuery } from "react-responsive";
 // components
 import Landscape from "./Landscape";
 import Portrait from "./Portrait";
 // assets
-import s from "./index.module.css";
+import darkTheme from "./dark.module.css";
+import lightTheme from "./light.module.css";
 
 /// Constants ///
 const validSubpaths = ["", "showcase", "me"];
@@ -15,12 +17,34 @@ function TabletOrMobile({ setTheme, theme }) {
   // initialize hook for responsive layout //
   const isLandscape = useMediaQuery({ minAspectRatio: "16/9" });
 
+  // initialize CSS module //
+  const [s, setS] = useState(theme === "light" ? lightTheme : darkTheme);
+
+  // change CSS module when theme is changed //
+  useEffect(() => {
+    setS(theme === "light" ? lightTheme : darkTheme);
+  }, [theme]);
+
   // get subpath //
   const subpath = window.location.pathname.split("/")[1];
 
   // render - invalid paths //
   if (!validSubpaths.includes(subpath)) {
-    return <div className={s.notFound}>Not Found</div>;
+    return (
+      <main
+        className={`${s.notFound} ${isLandscape ? s.landscape : s.portrait}`}
+      >
+        <p className={s.notFoundMessage}>
+          <span className={s.notFoundMessageLarge}>Oops!</span>
+          <span className={s.notFoundMessageSmall}>
+            This page does not exist.
+          </span>
+        </p>
+        <a className={s.notFoundLink} href="/">
+          Back to home
+        </a>
+      </main>
+    );
   }
 
   // render - main //
